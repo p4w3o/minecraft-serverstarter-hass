587b8dcbc71e163de97118039dbc63ebe14966b6
paweo <paveovdvk@gmail.com>
2026-03-27 18:15:06 +0100

    free port

--- minecraft_facade/app/facade.py
+++ minecraft_facade/app/facade.py
@@ -8,33 +8,31 @@ logger = logging.getLogger(__name__)
 
 class FacadeServer:
 
-    def __init__(self, config, ha):
-        self.config = config
-        self.ha = ha
-        self.running = False
-
     def start(self):
-
         port = self.config["port"]
 
-        self.sock = socket.socket()
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind(("0.0.0.0", port))
         self.sock.listen()
+        self.sock.settimeout(0.5)  # timeout pour exit rapide
 
         self.running = True
 
         threading.Thread(target=self.loop, daemon=True).start()
 
-        logger.info("Facade '%s' listening on %s",
-                    self.config["name"], port)
+        logger.info(f"Facade '{self.config['name']}' listening on {port}")
 
     def loop(self):
-
         proto = MinecraftProtocol(self.config, self.ha)
 
         while self.running:
-
-            conn, addr = self.sock.accept()
+            try:
+                conn, addr = self.sock.accept()
+            except socket.timeout:
+                continue
+            except OSError:
+                break  # socket fermé
 
             threading.Thread(
                 target=proto.handle,
@@ -43,10 +41,8 @@ class FacadeServer:
             ).start()
 
     def set_enabled(self, enabled):
-
         if enabled and not self.running:
             self.start()
-
         elif not enabled and self.running:
             self.running = False
             self.sock.close()
